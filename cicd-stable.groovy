1597c6be7678c040bb15048fc9dad6a4be3d5fd2
Mike Großmann <mgrossmann@users.noreply.github.com>
2026-02-08 10:21:10 +0100

    Update cicd-stable.groovy
    git url's corrected in cicd script
    
    Signed-off-by: Mike Großmann <mgrossmann@users.noreply.github.com>

--- cicd-stable.groovy
+++ cicd-stable.groovy
@@ -2,11 +2,11 @@ node('linux') {
   stage ('Poll') {
     checkout([
       $class: 'GitSCM', branches: [[name: '*/main']], extensions: [],
-      userRemoteConfigs: [[url: 'https://github.com/zopencommunity/hlasm_language_serverport.git']]])
+      userRemoteConfigs: [[url: 'https://github.com/zopencommunity/hlasm-language-serverport.git']]])
   }
   stage('Build') {
     build job: 'Port-Pipeline', parameters: [
-      string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/hlasm_language_serverport.git'),
+      string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/hlasm-language-serverport.git'),
       string(name: 'PORT_DESCRIPTION', value: 'A language server protocol (LSP) implemenation for the High Level Assembler language.'),
       string(name: 'BUILD_LINE', value: 'STABLE')
     ]
